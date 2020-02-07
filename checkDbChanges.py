ddd3e6f8d4238e53a7da238bbae041aaa233a5c9
panos3oesp <43377896+panos3oesp@users.noreply.github.com>
2020-02-07 15:03:16 +0200

    Create checkDbChanges.py

---
+++ checkDbChanges.py
@@ -0,0 +1,42 @@
+import pyodbc
+
+def connect2Db(server,database,username,password):
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';Direct=True;Database='+database+';UID='+username+';PWD='+password)
+    cursor = cnxn.cursor()
+    return cursor
+
+def getTableNames(cursor):
+    
+    sqlGetTables="SELECT TABLE_NAME as tablename "
+    sqlGetTables+=" FROM INFORMATION_SCHEMA.TABLES "
+    sqlGetTables+=" WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='softone' "
+    cursor.execute(sqlGetTables) 
+    return cursor.fetchall()
+
+def lastTableUpdateDateTime(cursor,tablename):
+    sql="SELECT OBJECT_NAME(OBJECT_ID) AS tablebaseName, last_user_update as lup,* "
+    sql+=" FROM sys.dm_db_index_usage_stats "
+    sql+=" WHERE database_id = DB_ID('softone') "
+    sql+=" AND OBJECT_ID=OBJECT_ID('"+tablename+"') "
+    
+    
+    cursor.execute(sql) 
+    return cursor.fetchone()
+    
+SERVER ="..."
+DB="..."
+USERNAME="..."
+PASSWORD="..."
+
+
+cursor = connect2Db(SERVER,DB,USERNAME,PASSWORD)
+
+tables = getTableNames(cursor)
+for table in tables:
+    lastUpdate = lastTableUpdateDateTime(cursor,table.tablename)
+    if lastUpdate and lastUpdate.lup:
+        print(table.tablename)
+        print(lastUpdate.lup)
+    
+    
+
